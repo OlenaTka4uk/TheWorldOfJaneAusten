9c6809221d3689422a94d1ff0650021cee6c4589
Olena Tkachuk <85578521+OlenaTka4uk@users.noreply.github.com>
2024-04-27 19:16:13 +0300

    Add DbContext, Connection string

--- TheWorldOfJaneAustenAPI/Program.cs
+++ TheWorldOfJaneAustenAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistense.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
